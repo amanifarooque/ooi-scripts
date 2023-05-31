61862925187c95af0fc3471f2274c198929ac9c1
Amani Farooque <amani.farooque@gmail.com>
2023-05-31 17:50:47 -0400

    Comment Data Cleaning Script

--- ui-comment-analysis.py
+++ ui-comment-analysis.py
@@ -8,7 +8,7 @@ from langdetect import detect
 
 
 # Read the comments data into Pandas data frame.
-df = pd.read_csv("input/ui-comments.csv")
+df = pd.read_csv("input/ui-comments-cleaned.csv")
 df.dropna(subset=['Comment'], inplace=True)
 
 # filter out non-english comments
@@ -32,7 +32,7 @@ vectorizer = TfidfVectorizer(stop_words=ui_stop_words, ngram_range=(1,3))
 X = vectorizer.fit_transform(documents)
 
 # set number of clusters
-true_k = 10
+true_k = 20
 
 # clustering model
 model = KMeans(n_clusters=true_k, init='k-means++', max_iter=1000, n_init=42, random_state=42)
@@ -58,10 +58,10 @@ df['cluster'] = model.predict(vectorizer.transform(df["Comment"]))
 df['cluster_label'] = df.cluster.map(topic_map)
 df['cluster_top_terms'] = df.cluster.map(topic_terms)
 
-df.to_csv("output/ui-comments-summary.csv", index=False, header=True)
+df.to_csv("output/ui-comments-categorized.csv", index=False, header=True, encoding='utf-8-sig')
 
 # Find the topic cluster on your site which are converting the best and also the topic which get impression but doesn't convert well.
 header = ['cluster_label', 'cluster_top_terms', 'cluster']
-df.groupby(['cluster_label', 'cluster_top_terms']).count().reset_index().sort_values(by=['cluster_label']).to_csv('output/ui-comments-clusters.csv',index=False, header=True, columns=header)
+df.groupby(['cluster_label', 'cluster_top_terms']).count().reset_index().sort_values(by=['cluster_label']).to_csv('output/ui-comments-clusters.csv',index=False, header=True, columns=header, encoding='utf-8-sig')
 
 
